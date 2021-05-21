15fc17542d77a3e46eb5750df96f2ed9b54808d2
evilgeniy <team.og.dota@gmail.com>
2021-05-21 19:08:30 +0300

    adding files

---
+++ 1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def fib(n):
+	if n == 0:
+             return 0
+     elif n == 1 or n == 2:
+             return 1
+    else:
+            return fib(n-1) + fib(n-2)
+def main():
+     logging.info(fib(10))
+     
+
+if __name__ == "__main__":
+     main()
+
---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.8
+
+ENV TARGETPATH=/home/ghost/Documents/ISP/LAb1/app/
+
+RUN mkdir -p "${TARGETPATH}"
+
+WORKDIR "${TARGETPATH}"
+
+COPY 1.py "${TARGETPATH}"
+
+CMD ["python3","1.py"]
+
